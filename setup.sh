42ef66ccbbbb210cc3a2c34c6c9171fc94a87249
Robert Holt <rholt@dattobackup.com>
2014-06-20 09:37:49 -0400

    Updated setup.sh to install required packages

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
 echo Installing packages...
-sudo yaourt --noconfirm -S vim tmux vim-airline-git ttf-powerline-fonts-git tmuxline-git tmuxline-git
+sudo yaourt --noconfirm -S zsh vim tmux zsh-syntax-highlighting vim-airline-git ttf-powerline-fonts-git tmuxline-git tmuxline-git
