8be9f452574abbd774d64b948e4d28158a22bcc9
Robert Holt <holt.r94@gmail.com>
2015-03-10 15:15:43 -0400

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -5,4 +5,11 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
 echo Installing packages...
-sudo yaourt --noconfirm -S zsh vim tmux zsh-syntax-highlighting vim-airline-git ttf-powerline-fonts-git tmuxline-git tmuxline-git tmux-mem-cpu-load-git
+packages="zsh vim tmux zsh-syntax-highlighting vim-airline-git ttf-powerline-fonts-git tmux-mem-cpu-load-git"
+if hash yaourt 2>/dev/null; then
+    yaourt --noconfirm -S $packages
+elif has pacaur 2>/dev/null; then
+    pacaur --noconfirm -S $packages
+else
+    echo AUR wrapper needed, istall yaourt or pacaur first
+fi
