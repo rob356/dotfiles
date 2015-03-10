0f2781d539770cb5e40f0bbb0cc7438471ef2a3c
Robert Holt <holt.r94@gmail.com>
2015-03-10 15:20:20 -0400

    Removed fonts package from setup.sh

--- setup.sh
+++ setup.sh
@@ -5,10 +5,10 @@ ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/oh-my-zsh ~/.oh-my-zsh
 echo Installing packages...
-packages="zsh vim tmux zsh-syntax-highlighting vim-airline-git ttf-powerline-fonts-git tmux-mem-cpu-load-git"
+packages="zsh vim tmux zsh-syntax-highlighting vim-airline-git tmux-mem-cpu-load-git"
 if hash yaourt 2>/dev/null; then
     yaourt --noconfirm -S $packages
-elif has pacaur 2>/dev/null; then
+elif hash pacaur 2>/dev/null; then
     pacaur --noconfirm -S $packages
 else
     echo AUR wrapper needed, istall yaourt or pacaur first
